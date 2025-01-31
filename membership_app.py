e833e7e3ca2c7bc6191b730dc73f351fd7b8dc23
Fernaldy Wirjowerdojo <faw@hehehehe.local>
2025-01-31 23:43:14 +0700

    Added membership display feature

--- membership_app.py
+++ membership_app.py
@@ -2,24 +2,38 @@ import os
 from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from obj.insert_db import insert_data_pg, MembershipExistsError
+from obj.db import connect_pg
 
 
 load_dotenv()
 
 membership_app = Flask(__name__)
-membership_app.config['UPLOAD_DIR'] = os.getenv('IMG_LOCAL_DIR')
+membership_app.config['UPLOAD_DIR'] = os.path.join('static', 'img')
+
+if not os.path.exists(membership_app.config['UPLOAD_DIR']):
+    os.makedirs(membership_app.config['UPLOAD_DIR'])
 
 @membership_app.route("/")
 def home():
     """Render index.html"""
-    return render_template('index.html')
+    conn = connect_pg()
+    try:
+        with conn.cursor() as cursor:
+            sql = 'select name, membership_number, membership_uri from memberships'
+            cursor.execute(sql)
+            memberships = cursor.fetchall()
+            memberships = [{'name': row[0], 'number': row[1], 'image': row[2]} for row in memberships]
+    finally:
+        conn.close()
+
+    return render_template('index.html', memberships=memberships)
 
 
 @membership_app.route("/upload", methods=['POST'])
 def fetch_user_upload():
     name = request.form.get('membership_name').upper()
     img = request.files.get('file')
-    number = request.form.get('number').upper()
+    number = request.form.get('number')
 
     if not name:
         return '<h1>Membership name is required.</h1>', 400
